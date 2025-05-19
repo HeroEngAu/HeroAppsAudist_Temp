26540c8183650950a7b89b3d98599000c1ec9848
HeroEngAu <mlevkovsky@heroengineering.com.au>
2025-05-19 09:25:59 +0800

    Initial commit

--- app/submit/[formUrl]/layout.tsx
+++ app/submit/[formUrl]/layout.tsx
@@ -3,8 +3,8 @@
 import Logo from "../../../components/Logo";
 import ThemeSwitcher from "../../../components/ThemeSwitcher";
 import { ReactNode } from "react";
-import { Amplify } from "aws-amplify"
-import outputs from "../../../../amplify_outputs.json"
+import { Amplify } from "aws-amplify";
+import outputs from "../../../amplify_outputs.json";
 
 Amplify.configure(outputs)
 
